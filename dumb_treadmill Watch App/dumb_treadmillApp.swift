3ebb6cce208a8e4a8453775a4c79f45979687b3e
samuel.lozier <samLozer@users.noreply.github.com>
2024-05-15 14:18:35 -0700

    Initial Commit

---
+++ dumb_treadmill Watch App/dumb_treadmillApp.swift
@@ -0,0 +1,17 @@
+//
+//  dumb_treadmillApp.swift
+//  dumb_treadmill Watch App
+//
+//  Created by Samuel Lozier on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct dumb_treadmill_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
